f6dd6bb90b717553fb3d6d4991c2c8d5727cf8ae
Rurek27 <2001.jaceksz@gmail.com>
2019-05-30 15:46:22 +0200

    dodałem nazwy funkcji

--- main/main.cpp
+++ main/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Hewoo world!" << endl;
+    cout<< "Naciœnij 1 aby wykonaæ dodawanie \n"
+    cout<< "Naciœnij 2 aby wykonaæ odejmowanie \n"
+    cout<< "Naciœnij 3 aby wykonaæ mno¿enie \n"
+    cout<< "Naciœnij 3 aby wykonaæ dzielenie \n"
+    cout<< "Naciœnij 4 any wykonaæ ci¹g Fibonaciego \n"
+    cout<< "Naciœnij 5 aby wykonaæ silnie \n"
     return 0;
 }
