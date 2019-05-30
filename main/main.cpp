04f67b2c96d2c56ec4b9c13335e8274b491b4226
unknown <mati13553@gmail.com>
2019-05-30 15:52:40 +0200

    Stworzono funkcje Fibbonachi

--- main/main.cpp
+++ main/main.cpp
@@ -2,12 +2,28 @@
 
 using namespace std;
 
-void fibbonachi(int fib){
+int fibbonachi(int fib){
+    int wyn1, wyn2, wyn3, clk;
+    wyn1=1;
+    wyn2=1;
+    clk=2;
+
+    if(fib==1 || fib==2){return 1;}
+
+    while (true){
+        wyn3=wyn1+wyn2;
+        wyn1=wyn2;
+        wyn2=wyn3;
+        clk++;
+
+        if(clk==fib){return(wyn3); break;}
+    }
 
 }
 
 int main()
 {
     cout << "Hewoo world!" << endl;
+
     return 0;
 }
