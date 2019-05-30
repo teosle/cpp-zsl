792a1645bcc8cbe09c167947b3a90365d43b7bac
unknown <tokonrad1@interia.pl>
2019-05-30 15:57:29 +0200

    dodanie matmy

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+
+/*komentarz bo komentarze sa wazne jak sie kod pisze*/
 
 using namespace std;
 
+
+    float dodawanie(float a, float b){
+        return a+b;
+    }
+
+    float odejmowanie(float a,float b){
+        return a+b;
+    }
+
+    float mnozenie(float a,float b){
+        return a*b;
+    }
+
+    float dzielenie(float a,float b){
+        return a/b;
+    }
+
+    float potegowanie(float a,float b){
+        return pow(a,b);
+    }
+
+
 int main()
 {
-    cout << "Hewoo world!" << endl;
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
